b090de7bc9816ef61a56ecbcaf16380eab503f6a
BeyondCloud <a1989796@yahoo.com.tw>
2016-06-28 23:14:34 +0800

    implement binary search x to key

--- BoardInputMgr.cpp
+++ BoardInputMgr.cpp
@@ -1,12 +1,15 @@
 // midiout.cpp
 #include <iostream>
 #include <cstdlib>
+#include <math.h>
 #include "RtMidi.h"
 #include <time.h>
 #define MAX_TOUCH 5
 #define KEYBOARD_WIDTH 540
 #define WIGHT_KEY_NUM 17
-#define VOLUME_SCALE 3
+#define TOTAL_KEY_NUM 29
+#define LOWEST_TONE 48
+#define VOLUME_SCALE 1
 const float WHIGHT_KEY_LENGTH = KEYBOARD_WIDTH/WIGHT_KEY_NUM; 
 RtMidiOut *midiout = new RtMidiOut();
 std::vector<unsigned char> message;
@@ -23,6 +26,15 @@ enum class Tone {C0,C0s,D0,D0s,E0,F0,F0s,G0,G0s,A0,A0s,B0,
 		 C9,C9s,D9,D9s,E9,F9,F9s,G9,G9s,A9,A9s,B9,
 		 C10,C10s,D10,D10s,E10,F10,F10s,G10,G10s,A10,A10s,B10,
 		};
+int keyJunctionPos[TOTAL_KEY_NUM];
+//unsigned int_8 total_key_num_log_ceil = ceil(log2(TOTAL_KEY_NUM));
+// C3 = 0~18, C3s = 18~36...
+void initKeyJunctionPosition()
+{
+    keyJunctionPos[0] = 0;
+    for(int i = 1;i < TOTAL_KEY_NUM;i++)
+	keyJunctionPos[i] = keyJunctionPos[i-1]+18;
+}
 struct blob {
     int x;
     int y;
@@ -38,112 +50,145 @@ void sleep(unsigned int mseconds)
     clock_t goal = mseconds + clock();
     while (goal > clock());
 }
+int xPositionToKey(int x)
+{
+	int end = TOTAL_KEY_NUM;
+	int start = 0;
+	int mid;
+   //binary search
+   while(end - start != 1)
+   {
+	   //fast start + floor((end-start)/2)
+	   mid = start + ((end-start)>>1);
+	   if( keyJunctionPos[mid] > x)
+		   end = mid;
+	   else 
+		   start = mid;
+   }
+    return start;
+}
 /*
 void update_input(blob (&b)[MAX_TOUCH])
 {
 	int play_tone;
 	for(int i=0 ;i<MAX_TOUCH;i++ )
 	{
-	    play_tone =(int)( b[i].x / WHIGHT_KEY_LENGTH );
-
-	    message[0] = 144 + i; //10010000 status byte 1001->note on  0000->cannel 0
-	    message[1] = 48 + play_tone;  //tone 
-	    message[2] = b[i].size * VOLUME_SCALE;  //volume
-	    midiout->sendMessage( &message );
+		
 	}
 
 }
 */
+unsigned int blobSizeToVolume(int blobsize)
+{
+    unsigned int volume = blobsize * VOLUME_SCALE ;
+    //fast version to determine if volume < 128
+    return ((volume >> 7)==0)? volume : 127 ;
+}
 void noteOn(int channel ,int tone,int volume)
 {
-  message[0] = 144 + channel;   //144 = 10010000 status byte 1001->note on  0000->cannel 0
-  message[1] = tone; 
-  message[2] = volume;
-  midiout->sendMessage( &message );
+    message[0] = 144 + channel;   //144 = 10010000 status byte 1001->note on  0000->cannel 0
+    message[1] = tone; 
+    message[2] = volume;
+    midiout->sendMessage( &message );
 }
 void noteOff(int channel ,int tone,int volume)
 {
-  message[0] = 128 + channel;   //144 = 10010000 status byte 1001->note on  0000->cannel 0
-  message[1] = tone; 
-  message[2] = volume;
-  midiout->sendMessage( &message );
+    message[0] = 128 + channel;   //144 = 10010000 status byte 1001->note on  0000->cannel 0
+    message[1] = tone; 
+    message[2] = volume;
+    midiout->sendMessage( &message );
 }
 void pitchBend(int channel ,int LSB,int MSB)
 {
-  message[0] = 224 + channel;   //144 = 10010000 status byte 1001->note on  0000->cannel 0
-  message[1] = LSB; 
-  message[2] = MSB;
-  midiout->sendMessage( &message );
+    message[0] = 224 + channel;   //144 = 10010000 status byte 1001->note on  0000->cannel 0
+    message[1] = LSB; 
+    message[2] = MSB;
+    midiout->sendMessage( &message );
 }
 void setPitchBendRange(int channel,int semitones,int cents)
 {
-  message[0] = 176 + channel; 
-  message[1] = 101; 
-  message[2] = 0; 
-  midiout->sendMessage( &message );
+    message[0] = 176 + channel; 
+    message[1] = 101; 
+    message[2] = 0; 
+    midiout->sendMessage( &message );
 
-  message[0] = 176 + channel; 
-  message[1] = 100;  
-  message[2] = 0; 
-  midiout->sendMessage( &message );
-//coarse -> ex: Bx 06 02 : set bend range +-2 semitone
-  message[0] = 176 + channel;
-  message[1] = 6; 
-  message[2] = semitones;  
-  midiout->sendMessage( &message );
-//fine 
-  message[0] = 176 + channel; 
-  message[1] = 38;  
-  message[2] = cents; 
-  midiout->sendMessage( &message );
+    message[0] = 176 + channel; 
+    message[1] = 100;  
+    message[2] = 0; 
+    midiout->sendMessage( &message );
+    //coarse -> ex: Bx 06 02 : set bend range +-2 semitone
+    message[0] = 176 + channel;
+    message[1] = 6; 
+    message[2] = semitones;  
+    midiout->sendMessage( &message );
+    //fine 
+    message[0] = 176 + channel; 
+    message[1] = 38;  
+    message[2] = cents; 
+    midiout->sendMessage( &message );
   
 }
 inline void setMainVolume(int channel,int volume)
 {
-  // Control Change: 176, 7, 100 (volume)
-  message[0] = 176 + channel; //10110000
-  message[1] = 7;  //7 means control main volume
-  message.push_back( volume );
-  midiout->sendMessage( &message );
+    // Control Change: 176, 7, 100 (volume)
+    message[0] = 176 + channel; //10110000
+    message[1] = 7;  //7 means control main volume
+    message.push_back( volume );
+    midiout->sendMessage( &message );
 }
 void selectInstrument(int channel,int instrument)
 {
-  // Send out a series of MIDI messages.
-  // Program change: 192, instrument , N/A
-  message.push_back( 192 + channel );
-  message.push_back( 16 ); //select instrument 1~128 see wiki
-  //third byte is not used for program change
-  midiout->sendMessage( &message );
+    // Send out a series of MIDI messages.
+    // Program change: 192, instrument , N/A
+    message.push_back( 192 + channel );
+    message.push_back( 16 ); //select instrument 1~128 see wiki
+    //third byte is not used for program change
+    midiout->sendMessage( &message );
 }
 int main()
 {
-  // Check available ports.
-  unsigned int nPorts = midiout->getPortCount();
-  if ( nPorts == 0 ) {
-    std::cout << "No ports available!\n";
-    goto cleanup;
-  }
- // std::cout <<  midiout->getPortName(1);
-  // port 0 : MS synthesizer
-  // port 1 : fuji_port
-  midiout->openPort( 0 );	
-  selectInstrument(1,16);
-  setMainVolume(1,100);
-  noteOn(1,(int)Tone::C4,100);
-  sleep(1000);
-  setPitchBendRange(1,12,0);
-  
-  for(int i=64 ;i<=127;i++)
-  {
+    initKeyJunctionPosition();
+	std::cout<<xPositionToKey(1)<<"\n";
+	std::cout<<xPositionToKey(9)<<"\n";
+	std::cout<<xPositionToKey(27)<<"\n";
+	
+	std::cout<<xPositionToKey(38)<<"\n";
+	std::cout<<xPositionToKey(49)<<"\n";
+	std::cout<<xPositionToKey(256)<<"\n";
+	std::cout<<xPositionToKey(321)<<"\n";
+	std::cout<<xPositionToKey(476)<<"\n";
+	
+	std::cout<<xPositionToKey(328)<<"\n";
+	/*
+    std::cout << blobSizeToVolume(126)<<"\n";
+    // Check available ports.
+    unsigned int nPorts = midiout->getPortCount();
+    if ( nPorts == 0 ) {
+	std::cout << "No ports available!\n";
+	goto cleanup;
+    }
+    // std::cout <<  midiout->getPortName(1);
+    // port 0 : MS synthesizer
+    // port 1 : fuji_port
+    midiout->openPort( 0 );	
+    selectInstrument(1,16);
+    setMainVolume(1,100);
+    noteOn(1,(int)Tone::C4,100);
+    sleep(1000);
+    setPitchBendRange(1,12,0);
+
+    for(int i=64 ;i<=127;i++)
+    {
       pitchBend(1,0,i);
       sleep( 20 ); // Platform-dependent ... see example in tests directory.
-  }
-  sleep(1000);
-  pitchBend(1,0,64);
-  noteOff(1,(int)Tone::C4,100);
-
-  // Clean up
- cleanup:
-  delete midiout;
-  return 0;
+    }
+    sleep(1000);
+    pitchBend(1,0,64);
+    noteOff(1,(int)Tone::C4,100);
+    */
+    // Clean up
+    cleanup:
+    delete midiout;
+    return 0;
+	
 }
