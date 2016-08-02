49730f6e4d3ae7f36cdaa06b94cdd770f6db3222
BeyondCloud <a1989796@yahoo.com.tw>
2016-08-02 23:33:02 +0800

    fix midi message push_back bug

--- src/Midi_IO.cpp
+++ src/Midi_IO.cpp
@@ -4,17 +4,18 @@
 #include "Midi_IO.h"
 #include "config.h"
 #define LZZ_INLINE inline
+
 Midi_IO::Midi_IO ()
 {
 	midiout = new RtMidiOut();
-    openPort( 0 );
-    selectInstrument(0,16);
-    for(int chnl =0;chnl<MAX_TOUCH;chnl++)
+    midiout->openPort( 0 );
+    for(int chnl =0;chnl<1;chnl++)
     {
-        setMainVolume(chnl,127);
+        selectInstrument(chnl,16);
+        setMainVolume(chnl,50);
         setExpression(chnl,127);
+        setPitchBendRange(chnl,BEND_RANGE_PM,0);
     }
-    setPitchBendRange(0,BEND_RANGE_PM,0);
 }
 void Midi_IO::openPort (int port)
 {
@@ -27,54 +28,62 @@ void Midi_IO::openPort (int port)
 		midiout->openPort( port );
 	}
 }
-void Midi_IO::setPitchBendRange (int channel, int semitones, int cents)
+void Midi_IO::setPitchBendRange (int chnl, int semitones, int cents)
 {
-	std::cout << "Channel "<< channel <<" pitch bend range set to +-"<< semitones <<"\n";
-	message[0] = 176 + channel;
-	message[1] = 101;
-	message[2] = 0;
-	midiout->sendMessage( &message );
-	message[0] = 176 + channel;
-	message[1] = 100;
-	message[2] = 0;
-	midiout->sendMessage( &message );
-	//coarse -> ex: Bx 06 02 : set bend range +-2 semitone
-	message[0] = 176 + channel;
-	message[1] = 6;
-	message[2] = semitones;
-	midiout->sendMessage( &message );
-	//fine
-	message[0] = 176 + channel;
-	message[1] = 38;
-	message[2] = cents;
-	midiout->sendMessage( &message );
+	std::cout << "Channel "<< chnl <<" pitch bend range set to +-"<< semitones <<"\n";
+    message.clear();
+    message.push_back(176+chnl);
+    message.push_back(101);
+    message.push_back(0);
+    midiout->sendMessage( &message );
+    message.clear();
+    message.push_back(176+chnl);
+    message.push_back(100);
+    message.push_back(0);
+    midiout->sendMessage( &message );
+    message.clear();
+    message.push_back(176+chnl);
+    message.push_back(6);
+    message.push_back(semitones);
+    midiout->sendMessage( &message );
+    message.clear();
+    message.push_back(176+chnl);
+    message.push_back(38);
+    message.push_back(cents);
+    midiout->sendMessage( &message );
 
 }
-void Midi_IO::setMainVolume (int channel, int volume)
+void Midi_IO::setMainVolume (int chnl,int volume)
 {
-		std::cout << "Channel "<< channel << " main volume set to "<< volume <<"\n";
-		message[0] = 176 + channel; //10110000
-		message[1] = 7;  //7 means control main volume
-		message[2] = volume;//0~127
-		midiout->sendMessage( &message );
+    message.clear();
+    message.push_back(176+chnl);
+    message.push_back(7);
+    message.push_back(volume);
+    midiout->sendMessage( &message );
+    std::cout <<"channel "<<chnl<< " main volume set to "<< volume <<"\n";
 }
-void Midi_IO::setExpression (int channel, int volume)
+void Midi_IO::setExpression (int chnl, int volume)
 {
-		std::cout << "Channel "<< channel <<" expression set to +-"<< volume <<"\n";
-		// Control Change: 176, 7, 100 (volume)
-		message[0] = 176 + channel; //10110000
-		message[1] = 11;
-		message[2] = volume;
-		midiout->sendMessage( &message );
+    message.clear();
+    std::cout << "Channel "<< chnl <<" expression set to "<< volume <<"\n";
+    message.push_back(176+chnl);
+    message.push_back(11);
+    message.push_back(volume);
+    midiout->sendMessage( &message );
 }
 void Midi_IO::selectInstrument (int channel, int instrument)
 {
-		std::cout << "Channel "<< channel <<" select instrument "<< instrument <<"\n";
-		// Send out a series of MIDI messages.
-		// Program change: 192, instrument , N/A
-		message.push_back( 192 + channel );
-		message.push_back( instrument ); //select instrument 1~128 see wiki
-		//third byte is not used for program change
-		midiout->sendMessage( &message );
+    message.clear();
+    // Program change: 192, instrument , N/A
+    message.push_back( 192 + channel );
+    message.push_back( instrument ); //select instrument 1~128 see wiki
+    //third byte is not used for program change
+    midiout->sendMessage( &message );
+    std::cout << "Channel "<< channel <<" select instrument "<< instrument <<"\n";
+}
+void Midi_IO::cleanUp()
+{
+    delete midiout;
 }
 #undef LZZ_INLINE
+
